91ff62cd79cf38ce97d6f8e29e29dcba10f223e8
Sophia Stephenson <stephenson3@wisc.edu>
2021-08-31 21:12:46 -0500

    link logo to homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,11 +9,13 @@ const Home = () => {
   return (
     <main>
       <nav className="navbar">
-        <img
-          src="https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1868&q=80"
-          alt="book in front of mountain"
-          style={{ width: 32, height: 32 }}
-        />
+        <a href="https://mern-your-books.herokuapp.com/">
+          <img
+            src="https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1868&q=80"
+            alt="book in front of mountain"
+            style={{ width: 32, height: 32 }}
+          />
+        </a>
       </nav>
       <div className="flex-row justify-center">
         {books &&
