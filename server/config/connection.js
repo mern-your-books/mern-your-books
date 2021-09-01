f8c65ce14bad144c018447f352e1c38d8976d367
Sophia Stephenson <stephenson3@wisc.edu>
2021-08-31 19:19:47 -0500

    set up configs folder

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/tech-friends-ins-demo',
+  process.env.MONGODB_URI ||
+    "mongodb+srv://stephenson36:mojo44@cluster0.n8ie9.mongodb.net/mern_db?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
