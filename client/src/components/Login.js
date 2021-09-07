9bf0fe65c6e84a7fc6756e3c61cfc4277913bb33
Nabiha Khan <nabiha.khan@me.com>
2021-09-07 12:10:09 -0700

    files location and some path edits

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,34 +1,71 @@
-import React from "react";
-// import image1 from "./book-homepage-image.jpg";
-import { useQuery } from "@apollo/client";
-import { QUERY_BOOKS } from "../utils/queries";
-import LoginPage from "../components/LoginPage/LoginPage";
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { Link } from 'react-router-dom';
+import { LOGIN } from '../utils/mutations';
+import Auth from '../utils/auth';
 
-const Login = () => {
-  const { loading, data } = useQuery(QUERY_BOOKS);
-  const books = data?.books || [];
+function Login(props) {
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN);
 
-  return <LoginPage />;
-};
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const mutationResponse = await login({
+        variables: { email: formState.email, password: formState.password },
+      });
+      const token = mutationResponse.data.login.token;
+      Auth.login(token);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  return (
+    <div className="container my-1">
+      <Link to="/signup">← Go to Signup</Link>
+
+      <h2>Login</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="email">Email address:</label>
+          <input
+            placeholder="youremail@test.com"
+            name="email"
+            type="email"
+            id="email"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="pwd">Password:</label>
+          <input
+            placeholder="******"
+            name="password"
+            type="password"
+            id="pwd"
+            onChange={handleChange}
+          />
+        </div>
+        {error ? (
+          <div>
+            <p className="error-text">The provided credentials are incorrect</p>
+          </div>
+        ) : null}
+        <div className="flex-row flex-end">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+}
 
 export default Login;
-
-// import React, { Component } from "react";
-// import { BrowserRouter as Router, Route } from "react-router-dom";
-// import LoginPage from "../components/LoginPage/LoginPage";
-
-// // const Login = () => <LoginPage />;
-
-// class Login extends Component {
-//   render() {
-//     return (
-//       <Router>
-//         <div className="Login">
-//           <Route path="/login" component={LoginPage} />
-//         </div>
-//       </Router>
-//     );
-//   }
-// }
-
-// export default Login;
